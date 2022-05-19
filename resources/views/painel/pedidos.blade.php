5b2811357f52c1a302546206b0dc0665e8affbee
Felipe Cruz <felipephplow@gmail.com>
2022-05-19 14:00:00 -0300

    a

--- resources/views/painel/pedidos.blade.php
+++ resources/views/painel/pedidos.blade.php
@@ -14,4 +14,56 @@
             <a href=""><button class="btn-filter-4"><span>Cancelado</span></button></a>
         </div>
     </div>
+    <div class="table-headling">
+        <div>
+            <div class="form-check">
+                <input class="form-check-input custom-checkbox" type="checkbox" value="" id="flexCheckDefault">
+            </div>
+        </div>
+        <div>
+            <select class="form-select" aria-label="Default select example">
+                <option selected>ID Pedido</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+        <div>
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Influencer</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+        <div>
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Cliente</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+        <div>
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Data</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+        <div>
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Status</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+    </div>
+    <div class="pedidos-body">
+        <div class="item-pedido">
+
+        </div>
+    </div>
 @endsection
