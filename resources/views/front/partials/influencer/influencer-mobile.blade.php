73d3e5debbfd8a236440099a36a7c52e6ee9c959
Felipe Cruz <felipephplow@gmail.com>
2022-06-09 10:30:23 -0300

    das

---
+++ resources/views/front/partials/influencer/influencer-mobile.blade.php
@@ -0,0 +1,114 @@
+<div class="m-influencer">
+    <div class="m-hero">
+        <div class="img-m-hero">
+            <img src="{{ asset('front/img/car-mobile.png') }}" alt="">
+        </div>
+        <div class="text-m-hero">
+            <h1>Consultas médicas a partir de <span>R$ 24</span></h1>
+        </div>
+        <div class="text-center">
+            <button class="btn btn-card" data-bs-toggle="modal" data-bs-target="#modalCard">Peça seu cartão</button>
+        </div>
+    </div>
+
+    <div class="m-hero-2 mt-5">
+        <div class="grid-hero-2">
+            <div>
+                <img src="{{ asset('front/img/calin.png') }}" alt="">
+            </div>
+            <div class="ps-1 pe-5">
+                <p><strong>Cartão com você</strong> é um novo cartão de descontos do Brasil. Ele oferece descontos de
+                    até 75%
+                    principalmente nas áreas de saúde, educação e lazer. Pagando um valor mensal, você pode aproveitar
+                    todos os benefícios e incluir seus filhos, marido ou esposa.</p>
+                <img src="{{ asset('front/img/agenda-new.svg') }}" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="partners">
+        <div>
+            <h1>Medicamentos<br> com até <span>35% de<br> desconto</span> </h1>
+        </div>
+        <div class="container">
+            <div class="slider-par">
+                <div>
+                    <img src="{{ asset('front/img/araujo.png') }}" alt="">
+                </div>
+                <div>
+                    <img src="{{ asset('front/img/onofre.png') }}" alt="">
+                </div>
+                <div>
+                    <img src="{{ asset('front/img/saopaulo.png') }}" alt="">
+                </div>
+                <div>
+                    <img src="{{ asset('front/img/drogasil.png') }}" alt="">
+                </div>
+                <div>
+                    <img src="{{ asset('front/img/ultrafarma.png') }}" alt="">
+                </div>
+                <div>
+                    <img src="{{ asset('front/img/raia.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="club-descontos">
+        <img src="{{ asset('front/img/clube.svg') }}" alt="">
+    </div>
+    <div class="club-1">
+        <img src="{{ asset('front/img/carlinhos-maia-1500x900.png') }}" alt="">
+    </div>
+    <div class="descontos">
+        <div class="container">
+            <div class="d-flex flex-column">
+                <div class="smile-grid">
+                    <div>
+                        <img src="{{ asset('front/img/smile.svg') }}" alt="">
+                    </div>
+                    <div>
+                        <h5>Apenas R$ 27,50 mensais pra toda a família</h5>
+                    </div>
+                </div>
+                <div class="smile-grid">
+                    <div>
+                        <img src="{{ asset('front/img/smile.svg') }}" alt="">
+                    </div>
+                    <div>
+                        <h5>Adicione até 7 dependentes
+
+                            (esposa ou marido e filhos)</h5>
+                    </div>
+                </div>
+                <div class="smile-grid">
+                    <div>
+                        <img src="{{ asset('front/img/smile.svg') }}" alt="">
+                    </div>
+                    <div>
+                        <h5>Pagamento com cartão de crédito ou pela conta de luz</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="call-bottom mt-5">
+        <h1>O cartão é fácil de pedir, fácil de pagar e fácil de usar!<br>
+            <span> Peça já o seu!</span>
+        </h1>
+    </div>
+    <div class="text-center my-4">
+        <button class="btn btn-card " data-bs-toggle="modal" data-bs-target="#modalCard">Peça seu cartão</button>
+    </div>
+    <div class="car">
+        <div>
+            <img src="{{ asset('front/img/car.png') }}" alt="">
+        </div>
+    </div>
+    <div class="call-bottom mt-5">
+        <h1>Seja nosso representante<br>
+            <span>e fature alto!</span>
+        </h1>
+    </div>
+    <div class="text-center my-4">
+        <button class="btn btn-card">Mais informações</button>
+    </div>
+</div>
